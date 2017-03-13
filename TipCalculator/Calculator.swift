ac5ef69473dd59340aa12bd0aa047b11248627fb
Kaushik Thekkekere <theka02@ca.com>
2017-03-12 23:03:34 -0700

    misc

--- TipCalculator/Calculator.swift
+++ TipCalculator/Calculator.swift
@@ -10,8 +10,7 @@ class Calculator: NSObject {
     
     func calculateTipAmount(billAmount:NSNumber,tipPercentage:NSNumber) -> NSNumber {
         let percentageTipAmount:NSNumber = NSNumber(value:(billAmount.floatValue * tipPercentage.floatValue)/100)
-//        let tipAmount = billAmount+percentageAmount;
-//        NSLog("Bill = %.2f tip =%.2f and Total Value %.2f",billAmount,tipPercentage, tipAmount);
+
         return percentageTipAmount;
     }
     
